d335d5c240729bda076d47a2d419d8a74361e7e0
Bernardo Vera <bvera.ruiz@gmail.com>
2023-06-01 18:07:07 +0200

    Initial Commit

---
+++ SmashBooking/SmashBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmashBookingApp.swift
+//  SmashBooking
+//
+//  Created by Bernardo Vera on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SmashBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
